cbef8be9daa6ea1814508692587c14a070cffecb
Darshan <darshan.smu@gmail.com>
2018-06-06 00:13:50 -0400

    F

--- 1.js
+++ 1.js
@@ -2,3 +2,5 @@ console.log("from master");
 console.log("from Develo and f1");
 console.log("from F1");
 console.log("from Develop and f2");
+console.log("from Develop new updated");
+console.log("from Develop new");
